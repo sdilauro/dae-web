8c947efed124a425711edaaa7b573758da8d1898
SebaDL <jsdilauro@gmail.com>
2023-10-16 16:25:22 -0300

    Wip: Responsive

--- src/App.tsx
+++ src/App.tsx
@@ -11,25 +11,45 @@ import { ColorModeSwitcher } from "./ColorModeSwitcher"
 import { DaoIcon } from "./components/DaoIcon"
 import { FoundationIcon } from "./components/FoundationIcon"
 import { Footer } from "./components/Footer"
+import { useEffect, useState } from "react"
 
-export const App = () => (
-  <ChakraProvider theme={theme} >
-    <Box width={"100%"} justifyContent={"end"} display={'flex'} padding={5}>
-      <ColorModeSwitcher justifySelf="flex-end" />
-    </Box>
-    <Box fontFamily={"monospace"} textAlign="left" fontSize="lg" width={"50%"} margin={'auto'}>
-      <Box display={"flex"} alignItems={"center"} mb={"1rem"} w={"100%"} justifyContent={"space-between"}>
-        <Box>
-          <Heading color={"#ff2d55"} fontFamily={"monospace"} textAlign={'left'}>Decentraland Alternative Explorers</Heading>
-          <Text textAlign={'left'}>Expanding Decentraland Explorers: Mobile & VR in Godot and Desktop in Bevy</Text>
-        </Box>
-        <Box display={"flex"} alignItems={"center"} marginLeft={'1rem'} minW={136}>
-          <Link
+
+
+
+export const App = () => {
+  const [width, setWidth] = useState<number>(window.innerWidth);
+
+  function handleWindowSizeChange() {
+    setWidth(window.innerWidth);
+  }
+  useEffect(() => {
+    window.addEventListener('resize', handleWindowSizeChange);
+    return () => {
+      window.removeEventListener('resize', handleWindowSizeChange);
+    }
+  }, []);
+
+  const isMobile = width <= 1024;
+  
+  
+  return (
+    <ChakraProvider theme={theme} >
+      <Box width={"100%"} justifyContent={"end"} display={'flex'} paddingX={5}>
+        <ColorModeSwitcher justifySelf="flex-end" />
+      </Box>
+      <Box fontFamily={"monospace"} textAlign="left" fontSize="lg" width={isMobile?"90%":"50%"} margin={'auto'}>
+        <Box display={"flex"} flexDirection={isMobile ? 'column-reverse' : 'row'} alignContent={""} alignItems={"center"} mb={"1rem"} w={"100%"} justifyContent={isMobile?"revert":"space-between"}>
+          <Box>
+            <Heading color={"#ff2d55"} fontFamily={"monospace"} textAlign={'left'}>Decentraland Alternative Explorers</Heading>
+            <Text textAlign={'left'}>Expanding Decentraland Explorers: Mobile & VR in Godot and Desktop in Bevy</Text>
+          </Box>
+          <Box display={"flex"} alignItems={"center"} marginLeft={isMobile?'0':'1rem'} marginBottom={isMobile?'1.5rem':'0'} minW={136}>
+            <Link
               href="https://decentraland.org"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <FoundationIcon h="60px" aspectRatio={1}/>
+              <FoundationIcon h="60px" aspectRatio={1} />
             </Link>
             <Link
               href="https://dao.decentraland.org/"
@@ -38,27 +58,28 @@ export const App = () => (
             >
               <DaoIcon h="76px" aspectRatio={1} />
             </Link>
+          </Box>
+        </Box>
+      
+        <Box >
+          <Image margin={'auto'} my={"3rem"} src='gibbresh.png' fallbackSrc='https://via.placeholder.com/850x350' />
+          <Text>After the goals achieved in the Protocol Squad we continue contributing to the Decentraland metaverse through the development of mobile clients with the Godot engine, oriented to Android and iOS platforms, with potential publication in App Store and Play Store and through the continuation of the development of the desktop client of the Bevy engine.</Text>
+          <Text>On the other hand, we contributed to the Decentraland documentation by working in collaboration with the Foundation. </Text>
+          <Text>For more info, you can see the{' '}
+            <Link
+            
+              href="https://dao.decentraland.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+              color={"#ff2d55"}>
+              proposal
+            </Link>
+            .
+          </Text>
         </Box>
       </Box>
-      
-      <Box >
-        <Image margin={'auto'} my={"3rem"} src='gibbresh.png' fallbackSrc='https://via.placeholder.com/850x350' />
-        <Text>After the goals achieved in the Protocol Squad we continue contributing to the Decentraland metaverse through the development of mobile clients with the Godot engine, oriented to Android and iOS platforms, with potential publication in App Store and Play Store and through the continuation of the development of the desktop client of the Bevy engine.</Text>
-        <Text>On the other hand, we contributed to the Decentraland documentation by working in collaboration with the Foundation. </Text>
-        <Text>For more info, you can see the{' '}
-          <Link
-            
-            href="https://dao.decentraland.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-            color={"#ff2d55"}>
-            proposal
-          </Link>
-          .
-        </Text>   
-      </Box>
-    </Box>
-    <Box h={'8rem'} w={'100%'} />
-    <Footer/>
-  </ChakraProvider>
-)
+      <Box h={'8rem'} w={'100%'} />
+      <Footer isMobile={isMobile} />
+    </ChakraProvider>
+  ) 
+}
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import {
-    HStack,
+    Stack,
     Link,
     Text
 } from "@chakra-ui/react"
@@ -7,12 +7,25 @@ import { FaDiscord, FaYoutube } from "react-icons/fa"
 import { BevyIcon } from "./BevyIcon"
 import { GodotIcon } from "./GodotIcon"
   
-  export const Footer = () => (
-      <HStack display={'flex'} spacing={'5rem'} justifyContent={'center'} align={'center'} bottom={0} position={'fixed'} w={'100%'} bgColor={'Background'}>
-          <Link display={'flex'} alignItems={'center'} href="https://www.youtube.com/playlist?list=PLBmhkGNdurqgEZ0JlaDew3Gdk-3xElgu7" target='_blank'><Text margin={'1rem'}>YouTube Playlist</Text><FaYoutube size={'1.5rem'}/></Link>
-          <Link display={'flex'} alignItems={'center'} href="https://github.com/decentraland/bevy-explorer" target='_blank'><Text margin={'1rem'}>Bevy Explorer</Text><BevyIcon h={'1.5rem'} /></Link>
-          <Link display={'flex'} alignItems={'center'} href="https://github.com/decentraland/godot-explorer" target='_blank'><Text margin={'1rem'}>Godot Explorer</Text><GodotIcon h={'1.5rem'} /></Link>
-          <Link display={'flex'} alignItems={'center'} href="https://discord.com/channels/894658869391933540/1058482438445215774" target='_blank'><Text margin={'1rem'}>DAO Discord Channel</Text><FaDiscord size={'1.5rem'}/></Link>
-        </HStack>
+type FooterProps = {isMobile: boolean}
+  export const Footer = (props:FooterProps) => (
+    <Stack direction={"row"} display={'flex'} spacing={props.isMobile?'0':'5rem'} justifyContent={'center'} align={'center'} bottom={0} position={'fixed'} w={'100%'} bgColor={'Background'}>
+          <Link m={"1rem"} display={'flex'} alignItems={'center'} href="https://www.youtube.com/playlist?list=PLBmhkGNdurqgEZ0JlaDew3Gdk-3xElgu7" target='_blank'>
+              {props.isMobile ? '' : <Text mr={'1rem'}>YouTube Playlist</Text>}
+              <FaYoutube size={'1.5rem'} />
+          </Link>
+          <Link m={"1rem"} display={'flex'} alignItems={'center'} href="https://github.com/decentraland/bevy-explorer" target='_blank'>
+              {props.isMobile ? '' : <Text mr={'1rem'}>Bevy Explorer</Text>}
+              <BevyIcon h={'1.5rem'} />
+          </Link>
+          <Link m={"1rem"} display={'flex'} alignItems={'center'} href="https://github.com/decentraland/godot-explorer" target='_blank'>
+              {props.isMobile ? '' : <Text mr={'1rem'}>Godot Explorer</Text>}
+              <GodotIcon h={'1.5rem'} />
+          </Link>
+          <Link m={"1rem"} display={'flex'} alignItems={'center'} href="https://discord.com/channels/894658869391933540/1058482438445215774" target='_blank'>
+              {props.isMobile ? '' : <Text mr={'1rem'}>DAO Discord Channel</Text>}
+              <FaDiscord size={'1.5rem'} />
+          </Link>
+    </Stack>
   )
   
